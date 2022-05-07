1700132c72bfcf8e570f67dca044ad773ec54d23
tuanio <nvatuan3@gmail.com>
2022-05-07 23:43:38 +0700

    remove printT

--- datamodule.py
+++ datamodule.py
@@ -130,7 +130,6 @@ class VivosDataModule(pl.LightningDataModule):
         # batch, time, feature
         specs = torch.nn.utils.rnn.pad_sequence(specs, batch_first=True)
         # specs = specs.unsqueeze(1)  # batch, channel, time, feature
-        print("Specs size", specs.size())
 
         trans = [self.text_process.text2int(s) for s in trans]
         trans = torch.nn.utils.rnn.pad_sequence(trans, batch_first=True).to(
